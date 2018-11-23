43c49cd6919b284eff60eaf0be95c1c865a4e68f
Elijah Pelofske <45249511+epelofske@users.noreply.github.com>
2018-11-22 21:25:20 -0700

    Update qubo_generators.py

--- qubo_generators.py
+++ qubo_generators.py
@@ -3,7 +3,7 @@ Each of these take an input of a Networkx graph object
 They return a list of linear terms h,
 and a dictionary of quadratic terms J
 """
-def maximum_clique_qubo(G):
+def maximum_clique_ising(G):
         h = []
         J = {}
         GC = nx.algorithms.operators.unary.complement(G)
@@ -14,3 +14,28 @@ def maximum_clique_qubo(G):
         for a in list(GC.edges()):
                 J[a] = 2
         return h, J
+def maximum_clique_qubo(G):
+        Q = {}
+        GC = nx.algorithms.operators.unary.complement(G)
+        for i in list(G.nodes()):
+                Q[(i, i)] = -1
+        for a in list(G.edges()):
+                Q[a] = 0
+        for a in list(GC.edges()):
+                Q[a] = 2
+        return Q
+def maximum_cut_ising(G):
+        J = {}
+        h = []
+        for a in list(G.edges()):
+                J[a] = 2
+        for i in list(G.nodes()):
+                h.append(-1*G.degree(i))
+        return h, J
+def maximum_cut_qubo(G):
+        Q = {}
+        for a in list(G.edges()):
+                Q[a] = 2
+        for i in list(G.nodes()):
+                Q[(i, i)] = -1*G.degree(i)
+        return Q
