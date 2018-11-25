9b75b85786646205885774ae10db1a12f4bc89ee
Elijah Pelofske <45249511+epelofske@users.noreply.github.com>
2018-11-25 14:49:05 -0700

    Update qubo_generators.py

--- qubo_generators.py
+++ qubo_generators.py
@@ -6,16 +6,31 @@ OR
 a dictionary, Q, of h and J combined
 """
 import networkx as nx
-def maximum_clique_ising(G):
+import dimod
+def bqm_ising_to_hj(dictionary):
+        hd = dictionary['linear']
+        J = dictionary['quadratic']
         h = []
-        J = {}
+        for a in hd:
+                h.append(hd[a])
+        return h, J
+def maximum_clique_qubo2(G):
+        Ql = {}
+        Qq = {}
         GC = nx.algorithms.operators.unary.complement(G)
         for i in list(G.nodes()):
-                h.append(-1)
+                Ql[i] = -1
         for a in list(G.edges()):
-                J[a] = 0
+                Qq[a] = 0
         for a in list(GC.edges()):
-                J[a] = 2
+                Qq[a] = 2
+        return Ql, Qq
+def maximum_clique_ising(G):
+        one, two = maximum_clique_qubo2(G)
+        bqm = dimod.BinaryQuadraticModel(one, two, 0.0, dimod.BINARY)
+        bqm_ising = bqm.change_vartype(dimod.SPIN, inplace=False)
+        ising_m = vars(bqm_ising)
+        h, J = bqm_ising_to_hj(ising_m)
         return h, J
 def maximum_clique_qubo(G):
         Q = {}
@@ -27,18 +42,26 @@ def maximum_clique_qubo(G):
         for a in list(GC.edges()):
                 Q[a] = 2
         return Q
-def maximum_independent_ising(G):
-        h = []
-        J = {}
+
+def maximum_independent_set_qubo2(G):
+        Ql = {}
+        Qq = {}
         GC = nx.algorithms.operators.unary.complement(G)
         for i in list(G.nodes()):
-                h.append(-1)
-        for a in list(GC.edges()):
-                J[a] = 0
+                Ql[i] = -1
         for a in list(G.edges()):
-                J[a] = 2
+                Qq[a] = 2
+        for a in list(GC.edges()):
+                Qq[a] = 0
+        return Ql, Qq
+def maximum_independent_ising(G):
+        one, two = maximum_indpendent_set_qubo2(G)
+        bqm = dimod.BinaryQuadraticModel(one, two, 0.0, dimod.BINARY)
+        bqm_ising = bqm.change_vartype(dimod.SPIN, inplace=False)
+        ising_m = vars(bqm_ising)
+        h, J = bqm_ising_to_hj(ising_m)
         return h, J
-def maximum_independent_qubo(G):
+def maximum_independent_set_qubo(G):
         Q = {}
         GC = nx.algorithms.operators.unary.complement(G)
         for i in list(G.nodes()):
@@ -48,14 +71,6 @@ def maximum_independent_qubo(G):
         for a in list(G.edges()):
                 Q[a] = 2
         return Q
-def maximum_cut_ising(G):
-        J = {}
-        h = []
-        for a in list(G.edges()):
-                J[a] = 2
-        for i in list(G.nodes()):
-                h.append(-1*G.degree(i))
-        return h, J
 def maximum_cut_qubo(G):
         Q = {}
         for a in list(G.edges()):
@@ -73,15 +88,5 @@ def vertex_cover_qubo(G):
         for a in list(G.nodes()):
                 Q[(a, a)] = -1
         return Q
-def vertex_cover_ising(G):
-        h = []
-        J = {}
-        GC = nx.algorithms.operators.unary.complement(G)
-        for a in list(G.edges()):
-                J[a] = 1
-        for i in list(GC.edges()):
-                J[a] = 0
-        for a in list(G.nodes()):
-                h.append(-1)
-        return h, J
+
 
