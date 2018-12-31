8de76076589483c52b54ddc24ccc97d8b5bc9eaa
Elijah Pelofske <45249511+epelofske@users.noreply.github.com>
2018-12-31 02:45:32 +0000

    Update qubo_ising_generators.py

--- qubo_ising_generators.py
+++ qubo_ising_generators.py
@@ -2,72 +2,16 @@
 Max Cut QUBO formulation from 
 https://www.sciencedirect.com/science/article/pii/S0166218X01003419 
 
-Maximum Clique, Maximum Independent Set, and Minimum Vertex Cover formluations from 
-https://arxiv.org/abs/1801.08649 and https://arxiv.org/abs/1302.5843 
+Minimum Vertex Cover Ising formluations from 
+https://arxiv.org/abs/1302.5843
+
+Maximum clique and maximum indpendent set from
+https://arxiv.org/pdf/1801.08649.pdf
 
 """
 import numpy as np
 import networkx as nx
-from dwave_qbsolv import QBSolv
-import dimod
 
-def bqm_ising_to_hj(dictionary):
-        hd = dictionary['linear']
-        J = dictionary['quadratic']
-        h = []
-        for a in hd:
-                h.append(hd[a])
-        return h, J
-def bqm_to_qubo(q):
-	lin = q['linear']
-	quad = q['quadratic']
-	out = {}
-	for a in lin:
-		quad[(a, a)] = lin[a]
-	return quad
-
-def maximum_clique_qubo2(G):
-        Ql = {}
-        Qq = {}
-        GC = nx.algorithms.operators.unary.complement(G)
-        for i in list(G.nodes()):
-                Ql[i] = -1
-        for a in list(GC.edges()):
-                Qq[a] = 2
-        return Ql, Qq
-def maximum_independent_set_qubo2(G):
-        Ql = {}
-        Qq = {}
-        GC = nx.algorithms.operators.unary.complement(G)
-        for i in list(G.nodes()):
-                Ql[i] = -1
-        for a in list(G.edges()):
-                Qq[a] = 2
-        return Ql, Qq
-def maximum_cut_qubo2(G):
-        Q = {}
-        Q1 = {}
-        for a in list(G.edges()):
-                Q[a] = 2
-        for i in list(G.nodes()):
-                Q1[i] = -1*G.degree(i)
-        return Q1, Q
-def vertex_cover_ising2(G):
-        J = {}
-        h = {}
-        for a in list(G.edges()):
-                J[a] = 2
-        for i in list(G.nodes()):
-                h[i] = (-1*G.degree(i))-1
-        return h, J
-
-def maximum_clique_ising_rigetti(G):
-        one, two = maximum_clique_qubo2(G)
-        bqm = dimod.BinaryQuadraticModel(one, two, 0.0, dimod.BINARY)
-        bqm_ising = bqm.change_vartype(dimod.SPIN, inplace=False)
-        ising_m = vars(bqm_ising)
-        h, J = bqm_ising_to_hj(ising_m)
-        return h, J
 def maximum_clique_qubo_rigetti(G):
         lin = []
         quad = {}
@@ -77,7 +21,7 @@ def maximum_clique_qubo_rigetti(G):
         for a in list(GC.edges()):
                 quad[a] = 2
         return lin, quad
-def maximum_clique_qubo(G):
+def maximum_clique_qubo_dwave(G):
         Q = {}
         GC = nx.algorithms.operators.unary.complement(G)
         for i in list(G.nodes()):
@@ -86,13 +30,6 @@ def maximum_clique_qubo(G):
                 Q[a] = 2
         return Q
 
-def maximum_independent_set_ising_rigetti(G):
-        one, two = maximum_independent_set_qubo2(G)
-        bqm = dimod.BinaryQuadraticModel(one, two, 0.0, dimod.BINARY)
-        bqm_ising = bqm.change_vartype(dimod.SPIN, inplace=False)
-        ising_m = vars(bqm_ising)
-        h, J = bqm_ising_to_hj(ising_m)
-        return h, J
 def maximum_independent_set_qubo_rigetti(G):
         lin = []
         quad = {}
@@ -101,7 +38,7 @@ def maximum_independent_set_qubo_rigetti(G):
         for a in list(G.edges()):
                 quad[a] = 2
         return lin, quad
-def maximum_independent_set_qubo(G):
+def maximum_independent_set_qubo_dwave(G):
         Q = {}
         for i in list(G.nodes()):
                 Q[(i, i)] = -1
@@ -109,13 +46,6 @@ def maximum_independent_set_qubo(G):
                 Q[a] = 2
         return Q
 
-def maximum_cut_ising_rigetti(G):
-        one, two = maximum_cut_qubo2(G)
-        bqm = dimod.BinaryQuadraticModel(one, two, 0.0, dimod.BINARY)
-        bqm_ising = bqm.change_vartype(dimod.SPIN, inplace=False)
-        ising_m = vars(bqm_ising)
-        h, J = bqm_ising_to_hj(ising_m)
-        return h, J
 def maximum_cut_qubo_rigetti(G):
         quad = {}
         lin = []
@@ -124,7 +54,7 @@ def maximum_cut_qubo_rigetti(G):
         for i in list(G.nodes()):
                 lin.append(-1*G.degree(i))
         return lin, quad
-def maximum_cut_qubo(G):
+def maximum_cut_qubo_dwave(G):
         Q = {}
         for a in list(G.edges()):
                 Q[a] = 2
@@ -132,7 +62,7 @@ def maximum_cut_qubo(G):
                 Q[(i, i)] = -1*G.degree(i)
         return Q
 
-def vertex_cover_ising_rigetti(G):
+def minimum_vertex_cover_ising_rigetti(G):
 	J = {}
 	h = []
 	for a in list(G.edges()):
@@ -140,27 +70,14 @@ def vertex_cover_ising_rigetti(G):
 	for i in list(G.nodes()):
 		h.append((-1*G.degree(i))-1)
 	return h, J
-def vertex_cover_qubo_rigetti(G):
-        one, two = vertex_cover_ising2(G)
-        bqm = dimod.BinaryQuadraticModel(one, two, 0.0, dimod.SPIN)
-        bqm_q = bqm.change_vartype(dimod.BINARY, inplace=False)
-        q = vars(bqm_q)
-        Q = bqm_to_qubo(q)
-        lin = []
-        quad = {}
-        for a in Q:
-                if a[0] == a[1]:
-                         lin.append(a[0])
-                else:
-                         quad[a] = Q[a]
-        return lin, quad
-def vertex_cover_qubo(G):
-        one, two = vertex_cover_ising2(G)
-        bqm = dimod.BinaryQuadraticModel(one, two, 0.0, dimod.SPIN)
-        bqm_q = bqm.change_vartype(dimod.BINARY, inplace=False)
-        q = vars(bqm_q)
-        Q = bqm_to_qubo(q)
-        return Q
+def minimum_vertex_cover_ising_dwave(G):
+	J = {}
+	h = []
+	for a in list(G.edges()):
+		J[a] = 2
+	for i in list(G.nodes()):
+		h.append((-1*G.degree(i))-1)
+	return h, J
 
 def max_cut_qubo_matrix_ibmqx(G):
         qubo = maximum_cut_qubo(G)
@@ -169,26 +86,8 @@ def max_cut_qubo_matrix_ibmqx(G):
                 data[a[0], a[1]] = qubo[a]
                 data[a[1], a[0]] = qubo[a]
         return data
-def max_cut_ising_matrix_ibmqx(G):
-        h, J = maximum_cut_ising_rigetti(G)
-        data = np.zeros((len(G), len(G)))
-        for a in J:
-                data[a[0], a[1]] = J[a]
-                data[a[1], a[0]] = J[a]
-        count = -1
-        for i in h:
-                count += 1
-                data[count, count] = i
-        return data
 
-def min_vertex_cover_qubo_matrix_ibmqx(G):
-        qubo = vertex_cover_qubo(G)
-        data = np.zeros((len(G), len(G)))
-        for a in qubo:
-                data[a[0], a[1]] = qubo[a]
-                data[a[1], a[0]] = qubo[a]
-        return data
-def min_vertex_cover_ising_matrix_ibmqx(G):
+def minimum_vertex_cover_ising_matrix_ibmqx(G):
         h, J = vertex_cover_ising_rigetti(G)
         data = np.zeros((len(G), len(G)))
         for a in J:
@@ -207,17 +106,6 @@ def max_independent_set_qubo_matrix_ibmqx(G):
                 data[a[0], a[1]] = qubo[a]
                 data[a[1], a[0]] = qubo[a]
         return data
-def max_independent_set_ising_matrix_ibmqx(G):
-        h, J = maximum_independent_set_ising(G)
-        data = np.zeros((len(G), len(G)))
-        for a in J:
-                data[a[0], a[1]] = J[a]
-                data[a[1], a[0]] = J[a]
-        count = -1
-        for i in h:
-                count += 1
-                data[count, count] = i
-        return data
 
 def max_clique_qubo_matrix_ibmqx(G):
         qubo = maximum_clique_qubo(G)
@@ -225,15 +113,4 @@ def max_clique_qubo_matrix_ibmqx(G):
         for a in qubo:
                 data[a[0], a[1]] = qubo[a]
                 data[a[1], a[0]] = qubo[a]
-        return data
-def max_clique_ising_matrix_ibmqx(G):
-        h, J = maximum_clique_ising_rigetti(G)
-        data = np.zeros((len(G), len(G)))
-        for a in J:
-                data[a[0], a[1]] = J[a]
-                data[a[1], a[0]] = J[a]
-        count = -1
-        for i in h:
-                count += 1
-                data[count, count] = i
         return data
