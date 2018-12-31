19a626e649e1eb9fda0f17191082dd838c39d735
Elijah Pelofske <45249511+epelofske@users.noreply.github.com>
2018-12-31 03:18:59 +0000

    Update rigetti_maxcut_noise.py

--- rigetti_maxcut_noise.py
+++ rigetti_maxcut_noise.py
@@ -1,17 +1,8 @@
-from quil2qasm import *
 from ibmqx_ising_qubo_qaoa import *
 from rigetti_ising_qubo_qaoa import *
 from qubo_ising_generators import *
-from pyquil.api import get_qc
 import networkx as nx
-import numpy as np
-from dwave_qbsolv import QBSolv
-import dimod
 from graphs import *
-#from dwave.system.samplers import DWaveSampler
-from qiskit import *
-from qiskit_aqua.components.optimizers import *
-from qiskit_aqua import QuantumInstance
 from classical_solvers import *
 
 def combined_test2():
@@ -34,5 +25,3 @@ def combined_test2():
                 #         qubo.append(0)
                 print(x, qubo)
 print(combined_test2())
-
-
