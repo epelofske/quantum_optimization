8c2e384224e4cf3d72b12665a4e45e2e9f043a18
Elijah Pelofske <45249511+epelofske@users.noreply.github.com>
2018-12-31 05:34:09 +0000

    Add files via upload

---
+++ ibmqx_mc.py
@@ -0,0 +1,50 @@
+from graphs import *
+from quil2qasm import *
+from ibmqx_ising_qubo_qaoa import *
+from rigetti_ising_qubo_qaoa import *
+from qubo_ising_generators import *
+from pyquil.api import get_qc
+import networkx as nx
+import numpy as np
+from dwave_qbsolv import QBSolv
+import dimod
+#from dwave.system.samplers import DWaveSampler
+from qiskit import *
+from qiskit_aqua.components.optimizers import *
+from qiskit_aqua import QuantumInstance
+from classical_solvers import *
+import warnings
+from graphs import *
+warnings.filterwarnings("ignore")
+
+def combined_test():
+        #G = nx.gnp_random_graph(7, 0.5, 101)
+        G = nx.algorithms.operators.unary.complement(melbourne())
+        ising = []
+        ising2 = []
+        x = []
+        print('ibmqx, powell, qasm simulator, no noise, complement of melbourne')
+        print(maximum_clique(G))
+        for a in range(1, 50):
+             x.append(a)
+             optimizer = POWELL()
+             result_out = solve_ibmqx_ising_qubo(G, max_clique_qubo_matrix_ibmqx, optimizer, a)
+
+             result2 = []
+             for i in result_out:
+                  if i == 0:
+                        result2.append(1)
+                  if i == 1:
+                        result2.append(0)
+
+             ref = subg_is_clique(result2, G)
+             if ref == True:
+                  ising2.append(result2.count(1))
+             else:
+                  ising2.append(0)
+
+             print(x, ising2)
+
+print(combined_test())
+
+
