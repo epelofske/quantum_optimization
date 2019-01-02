fc90f75a151eede5e9e27d3e5c0814bda90d9fbc
Elijah Pelofske <45249511+epelofske@users.noreply.github.com>
2019-01-02 10:43:12 +0000

    Update rigetti_mvc_no_noise.py

--- rigetti_mvc_no_noise.py
+++ rigetti_mvc_no_noise.py
@@ -4,24 +4,32 @@ from qubo_ising_generators import *
 import networkx as nx
 from graphs import *
 from classical_solvers import *
+import time
 
-def combined_test():
+def combined_test2():
         #G = melbourne()
-        G = nx.gnp_random_graph(10, 0.5, 101)
-        top = '10q-qvm'
+        G = nx.gnp_random_graph(7, 0.5, 101)
+        top = '7q-qvm'
         opt = 'POWELL'
         x = []
         ising = []
         qubo = []
-        print('Powell, gnp topology, minimum vertex cover, 10q rigetti qvm no noise')
+        timer = []
+        print('Powell, gnp topology, minimum vertex cover, 7q rigetti qvm no noise')
         print(minimum_vertex_cover(G))
         for a in range(1, 50):
                 x.append(a)
-                result_out = rigetti_ising_qubo(G, minimum_vertex_cover_ising_rigetti, opt, top, a)
+                start1 = time.clock()
+                start2 = time.time()
+                result_out = rigetti_ising_qubo(G, vertex_cover_ising_rigetti, opt, top, a)
+                diff2 = time.time()-start2
+                diff1 = time.clock()-start1
+                timer.append([diff1, diff2])
                 ref = is_vertex_cover(result_out, G)
                 if ref == True:
                          ising.append(result_out.count(1))
                 else:
                          ising.append(0)
-                print(x, ising)
-print(combined_test())
+                print(x, ising, timer)
+print(combined_test2())
+
