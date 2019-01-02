f4b8208d332cb3a53e3079080f7146f79eaeec0f
Elijah Pelofske <45249511+epelofske@users.noreply.github.com>
2019-01-02 10:42:33 +0000

    Update rigetti_maxcut_noise.py

--- rigetti_maxcut_noise.py
+++ rigetti_maxcut_noise.py
@@ -4,24 +4,27 @@ from qubo_ising_generators import *
 import networkx as nx
 from graphs import *
 from classical_solvers import *
+import time
 
 def combined_test2():
         #G = melbourne()
-        G = nx.gnp_random_graph(10, 0.5, 101)
-        top = '10q-noisy-qvm'
+        G = nx.gnp_random_graph(7, 0.5, 101)
+        top = '7q-noisy-qvm'
         opt = 'POWELL'
         x = []
         ising = []
         qubo = []
-        print('Powell, gnp topology, maximum cut, 10q rigetti qvm noise')
-        print(20)
+        timer = []
+        print('Powell, gnp topology, maximum cut, 7q rigetti qvm noise')
         for a in range(1, 50):
                 x.append(a)
+                start1 = time.clock()
+                start2 = time.time()
                 result_out = rigetti_ising_qubo(G, maximum_cut_qubo_rigetti, opt, top, a)
-                #ref = subg_is_clique(result_out, G)
-                #if ref == True:
+                diff2 = time.time()-start2
+                diff1 = time.clock()-start1
+                timer.append([diff1, diff2])
                 qubo.append(max_cut_value(result_out, G))
-                #else:
-                #         qubo.append(0)
-                print(x, qubo)
+                print(x, qubo, timer)
 print(combined_test2())
+
