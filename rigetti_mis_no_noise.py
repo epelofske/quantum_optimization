d69eceadba52dd65bd0b3be79a4b60da55e1052f
Elijah Pelofske <45249511+epelofske@users.noreply.github.com>
2019-01-02 10:40:59 +0000

    Update rigetti_mis_no_noise.py

--- rigetti_mis_no_noise.py
+++ rigetti_mis_no_noise.py
@@ -4,24 +4,32 @@ from qubo_ising_generators import *
 import networkx as nx
 from graphs import *
 from classical_solvers import *
+import time
 
-def combined_test():
+def combined_test2():
         #G = melbourne()
-        G = nx.gnp_random_graph(10, 0.5, 101)
-        top = '10q-qvm'
+        G = nx.gnp_random_graph(7, 0.5, 101)
+        top = '7q-qvm'
         opt = 'POWELL'
         x = []
         ising = []
         qubo = []
-        print('Powell, gnp topology, maximum independent set, 10q rigetti qvm no noise')
+        timer = []
+        print('Powell, gnp topology, maximum independent set, 7q rigetti qvm no noise')
         print(maximum_independent_set(G))
         for a in range(1, 50):
                 x.append(a)
+                start1 = time.clock()
+                start2 = time.time()
                 result_out = rigetti_ising_qubo(G, maximum_independent_set_qubo_rigetti, opt, top, a)
+                diff2 = time.time()-start2
+                diff1 = time.clock()-start1
+                timer.append([diff1, diff2])
                 ref = is_independent_set(result_out, G)
                 if ref == True:
                          qubo.append(result_out.count(1))
                 else:
                          qubo.append(0)
-                print(x, qubo)
-print(combined_test())
+                print(x, qubo, timer)
+print(combined_test2())
+
