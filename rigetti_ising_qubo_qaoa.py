b4294a78e0cf00621380aec8c99083c99faa2e1f
Elijah Pelofske <45249511+epelofske@users.noreply.github.com>
2019-01-15 04:10:19 +0000

    Update rigetti_ising_qubo_qaoa.py

--- rigetti_ising_qubo_qaoa.py
+++ rigetti_ising_qubo_qaoa.py
@@ -24,16 +24,8 @@ def rigetti_ising_qubo(G, func, optimizer, comp, p):
         qc = get_qc(comp)
         h, J = func(G)
         with suppress_stdout():
-                x, z = ising(h, J, num_steps=p, connection=qc, samples = 100, minimizer_kwargs = {'method': optimizer})
+                x, energ, z = ising(h, J, num_steps=p, connection=qc, minimizer_kwargs = {'method': optimizer})
         """
         possible methods: COBYLA, SLSQP, TNC, CG, BFGS, Powell, Nelder-Mead, L-BFGS-B
         """
         return x
-        out = unique(x)
-        result_out = {}
-        for a in out:
-                temp = ''
-                for inter_ in a:
-                        temp += str(inter_)
-                result_out[temp] = x.count(a)
-        return result_out
