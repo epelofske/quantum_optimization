0c697393d04b6d3b6676fda14f2cc1b9b6c209bb
Elijah Pelofske <45249511+epelofske@users.noreply.github.com>
2019-01-17 06:43:57 +0000

    Update rigetti_mvc_noise.py

--- rigetti_mvc_noise.py
+++ rigetti_mvc_noise.py
@@ -8,20 +8,20 @@ import time
 
 def combined_test2():
 #        G = melbourne()
-        G = nx.gnp_random_graph(7, 0.5, 101)
-        top = '7q-noisy-qvm'
-        opt = 'POWELL'
+        G = nx.gnp_random_graph(8, 0.5, 101)
+        top = '8q-noisy-qvm'
+        opt = 'SLSQP'
         x = []
         ising = []
         qubo = []
         timer = []
-        print('Powell, gnp topology, minimum vertex cover, 7q rigetti qvm noise')
+        print('SLSQP, gnp, minimum vertex cover, 8q rigetti qvm noise')
         print(minimum_vertex_cover(G))
         for a in range(1, 50):
                 x.append(a)
                 start1 = time.clock()
                 start2 = time.time()
-                result_out = rigetti_ising_qubo(G, minimum_vertex_cover_ising_rigetti, opt, top, a)
+                result_out = rigetti_ising_qubo(G, minimum_vertex_cover_qubo_rigetti, opt, top, a)
                 diff2 = time.time()-start2
                 diff1 = time.clock()-start1
                 timer.append([diff1, diff2])
@@ -30,6 +30,5 @@ def combined_test2():
                          ising.append(result_out.count(1))
                 else:
                          ising.append(0)
-                print(x, ising, timer)
+                print(x, ising)
 print(combined_test2())
-
