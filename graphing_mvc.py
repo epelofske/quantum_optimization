dd0af718e2a80bc0445caf969a0cbc9ba33bcace
Elijah Pelofske <45249511+epelofske@users.noreply.github.com>
2019-01-18 05:06:29 +0000

    Update graphing_mvc.py

--- graphing_mvc.py
+++ graphing_mvc.py
@@ -6,14 +6,21 @@ fig, ax = plt.subplots()
 #ax = subplot(1,1,1)
 #G = nx.gnp_random_graph(7, 0.5, 101)
 #nx.draw(G, with_labels=True)
-x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29]
 #ibmqx = [8, 8, 10, 9, 9, 8, 8, 8, 8, 8, 8, 10, 8, 8, 7, 8, 10, 8, 8, 8, 8, 8, 8, 8, 8, 6, 8, 7, 8]
-ibmqx = [7, 0, 6, 6, 6, 5, 4, 5, 0, 0, 6, 4, 4, 4, 0, 5, 6, 5, 4, 5, 5, 0, 6, 5, 0, 0, 5, 5, 0]
-
 #qvm_noise = [5, 0, 0, 8, 6, 8, 9, 8, 8, 9, 6, 8, 7, 8, 6, 5, 2, 7, 3, 5, 6, 6, 0, 7, 7, 0, 7, 4, 7]
 #qvm_no = [8, 0, 7, 8, 2, 7, 4, 8, 8, 8, 2, 8, 8, 8, 6, 10, 9, 6, 6, 8, 8, 7, 8, 9, 8, 5, 8, 5, 8]
+"""
+x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29]
+ibmqx = [7, 0, 6, 6, 6, 5, 4, 5, 0, 0, 6, 4, 4, 4, 0, 5, 6, 5, 4, 5, 5, 0, 6, 5, 0, 0, 5, 5, 0]
 qvm_no = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 qvm_noise = [0, 0, 0, 0, 0, 0, 6, 0, 0, 0, 0, 0, 5, 7, 0, 0, 0, 0, 6, 6, 0, 0, 0, 0, 0, 0, 6, 0, 0]
+"""
+x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32]
+ibmqx = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 6, 0, 0, 6, 0, 0, 0, 0, 0, 0, 7, 0, 0]
+qvm_no = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+qvm_noise = [0, 0, 0, 0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 5, 0, 0, 6, 0, 6, 0, 0, 0, 6, 4, 0, 0, 0, 0, 0]
+print(len(ibmqx), len(qvm_no), len(qvm_noise))
+
 y = []
 for a in x:
 	y.append(4)
@@ -36,12 +43,12 @@ for a in qvm_noise:
         else:
                 qvm_noise2.append(a)
 
-plt.title('POWELL optimizer, Maximum Vertex Cover, Graph gnp(7, 0.5, 101)', fontsize=20)
+plt.title('SLSQP optimizer, Maximum Vertex Cover, Graph gnp(7, 0.5, 101)', fontsize=20)
 plt.plot(x, ibmqx2, 'og-', label='qasm_simulator no noise')
 plt.plot(x, qvm_noise2, 'ob-', label='qvm noise')
 plt.plot(x, qvm_no2, 'or-', label='qvm no noise')
 plt.plot(x, y, 'm-', label='minimum vertex cover value')
-plt.xlabel('Trotterization order', fontsize=20)
+plt.xlabel('Number of Steps', fontsize=20)
 plt.ylabel('Found Solution Size', fontsize=20)
 plt.legend(loc='upper right')
 
