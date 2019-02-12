6e3939ccd014f5dcdd499931b8109811024f2716
Elijah Pelofske <45249511+epelofske@users.noreply.github.com>
2019-02-12 13:43:30 -0700

    Update max_cut_ibmqx_average_accuracy.py

--- max_cut_ibmqx_average_accuracy.py
+++ max_cut_ibmqx_average_accuracy.py
@@ -8,27 +8,24 @@ import time
 import warnings
 warnings.filterwarnings("ignore")
 
-def ibqmx_qaoa(G, optimizer, func):
-        x = []
-        timer_main = []
+def ibqmx_qaoa(G, optimizer, func, r):
+        app_ratio = 0
         qubo_main = []
-        for a in range(1, (len(G)*2)+1):
-            x.append(a)
-            qubo = []
-            timer = []
-                start1 = time.clock()
-                result_out = solve_ibmqx_ising_qubo(G, func, SLSQP(), a)
-                diff1 = time.clock()-start1
-                timer.append(diff1)
-                result2 = []
-                for i in result_out:
+        counter = 0
+        for avg in range(0, 1000):
+            counter += 1
+            result_out = solve_ibmqx_ising_qubo(G, func, optimizer, r)
+            result2 = []
+            for i in result_out:
                   if i == 0:
                         result2.append(1)
                   if i == 1:
                         result2.append(0)
-                qubo.append(max_cut_value(result2, G))
-
+            x = max_cut_value(result2, G)
+            qubo_main.append(x)
+            app_ratio += x
+            print(float(app_ratio)/float(counter))
 G = nx.gnp_random_graph(7, 0.5, 101)
 print('maxcut')
 for op in opts:
-  print(op, ibqmx_qaoa(G, op, max_cut_qubo_matrix_ibmqx))
+  print(ibqmx_qaoa(G, SLSQP(), max_cut_qubo_matrix_ibmqx, 1))
