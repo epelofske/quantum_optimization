8be3fabd676ce29017716ee572bee6efa4f55a9d
Elijah Pelofske <45249511+epelofske@users.noreply.github.com>
2019-02-13 08:48:48 -0700

    Create min_vc_random_average_accuracy.py

---
+++ min_vc_random_average_accuracy.py
@@ -0,0 +1,25 @@
+import networkx as nx
+import random
+from classical_solvers import *
+
+def rand_partition(G):
+    sampl = list(G.nodes())
+    length = random.randint(0, max(sampl))
+    part1 = random.sample(sampl, length)
+    return part1
+
+out = []
+avg = 0
+counter = 0
+for i in range(0, 100):
+  counter += 1
+  G = nx.gnp_random_graph(7, 0.5, 101)
+  subg1 = rand_partition(G)
+  H = G.subgraph(subg1)
+  val = is_vertex_cover(H)
+  if val == True:
+    out.append(len(H))
+    avg += val
+  else:
+    out.append(len(G))
+  print((float(avg)/float(counter))/float(max(out)))
