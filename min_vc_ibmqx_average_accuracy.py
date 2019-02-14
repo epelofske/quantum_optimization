618309b2ae736862a953e17607d990afa177e72c
Elijah Pelofske <45249511+epelofske@users.noreply.github.com>
2019-02-14 12:24:14 -0700

    Create min_vc_ibmqx_average_accuracy.py

---
+++ min_vc_ibmqx_average_accuracy.py
@@ -0,0 +1,36 @@
+
+from utils.graphs import *
+from utils.ibmqx_ising_qubo_qaoa import *
+from utils.qubo_ising_generators import *
+from utils.classical_solvers import *
+import networkx as nx
+import numpy as np
+import time
+import warnings
+warnings.filterwarnings("ignore")
+
+def ibqmx_qaoa(G, optimizer, func, r):
+        app_ratio = 0
+        qubo_main = []
+        counter = 0
+        for avg in range(0, 100):
+            counter += 1
+            result_out = solve_ibmqx_ising_qubo(G, func, optimizer, r)
+            result2 = []
+            for i in result_out:
+                  if i == 0:
+                        result2.append(1)
+                  if i == 1:
+                        result2.append(0)
+            H = G.subgraph(result2)
+            if is_vertex_cover(H) == True:
+              qubo_main.append(len(H))
+              app_ratio += len(H)
+            else:
+              qubo_main.append(len(G))
+              app_ratio += len(G)
+            print(float(app_ratio)/float(counter))
+G = nx.gnp_random_graph(7, 0.5, 101)
+print('min vc')
+for op in opts:
+  print(ibqmx_qaoa(G, SLSQP(), minimum_vertex_cover_qubo_matrix_ibmqx, 1))
