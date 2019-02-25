0b568104f36a788fce839c80748f6e1b2a838c80
Elijah Pelofske <45249511+epelofske@users.noreply.github.com>
2019-02-25 02:17:11 -0700

    Add files via upload

---
+++ ibmqx_maxcut_nisq_ibmqx.py
@@ -0,0 +1,44 @@
+from utils.graphs import *
+from utils.ibmqx_ising_qubo_qaoa import *
+from utils.qubo_ising_generators import *
+from utils.classical_solvers import *
+import networkx as nx
+import numpy as np
+import time
+import warnings
+warnings.filterwarnings("ignore")
+
+def combined_test():
+        #G = nx.gnp_random_graph(7, 0.5, 101)
+#        G = melbourne()
+        G = ibmqx4()
+        #G1 = nx.Graph()
+        #G1.add_edge(0, 1)
+        #G1.add_edge(1, 2)
+        #G1.add_edge(2, 3)
+        #G1.add_edge(0, 3)
+        #G = nx.algorithms.operators.unary.complement(G1)
+        ising = []
+        ising2 = []
+        x = []
+        print('ibmqx, SLSQP, ibmqx4')
+        #print(maximum_clique(G))
+        for a in range(1, 20):
+             x.append(a)
+#             optimizer = POWELL()
+             optimizer = SLSQP()
+             result_out = solve_ibmqx_ising_qubo_nisq_ibmqx4(G, max_cut_qubo_matrix_ibmqx, optimizer, a)
+
+             result2 = []
+             for i in result_out:
+                  if i == 0:
+                        result2.append(1)
+                  if i == 1:
+                        result2.append(0)
+
+             ising2.append(max_cut_value(result2, G))
+             print(x, ising2)
+
+print(combined_test())
+
+
