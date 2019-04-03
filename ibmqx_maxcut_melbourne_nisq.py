dab9a388cb39f3e6c87ca0227072abbf9478c8ea
Elijah Pelofske <45249511+epelofske@users.noreply.github.com>
2019-04-03 09:05:47 -0600

    Update and rename ibmqx_maxcut_nisq.py to ibmqx_maxcut_melbourne_nisq.py

--- ibmqx_maxcut_melbourne_nisq.py
+++ ibmqx_maxcut_melbourne_nisq.py
@@ -9,28 +9,21 @@ import warnings
 warnings.filterwarnings("ignore")
 
 def combined_test():
-        #G = nx.gnp_random_graph(7, 0.5, 101)
         G = melbourne()
-        #G = nx.algorithms.operators.unary.complement(G1)
         ising = []
         ising2 = []
         x = []
         print('ibmqx, SLSQP, ibmq 16 melbourne solver, melbourne')
-        #print(maximum_clique(G))
         for a in range(1, 20):
              x.append(a)
-#             optimizer = POWELL()
              optimizer = SLSQP()
              result_out = solve_ibmqx_ising_qubo_nisq_melbourne(G, max_cut_qubo_matrix_ibmqx, optimizer, a)
-
              result2 = []
              for i in result_out:
                   if i == 0:
                         result2.append(1)
                   if i == 1:
                         result2.append(0)
-
              ising2.append(max_cut_value(result2, G))
              print(x, ising2)
-
 print(combined_test())
