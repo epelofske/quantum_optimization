d24b0d448c831a64778c7ae1e967dc6ff93b1af2
Elijah Pelofske <45249511+epelofske@users.noreply.github.com>
2019-04-03 09:06:54 -0600

    Update and rename ibmqx_max_clique_nisq.py to nisq/ibmqx_max_clique_nisq.py

--- nisq/ibmqx_max_clique_nisq.py
+++ nisq/ibmqx_max_clique_nisq.py
@@ -19,21 +19,17 @@ def combined_test():
              x.append(a)
              optimizer = SLSQP()
              result_out = solve_ibmqx_ising_qubo_nisq_ibmqx4(G, max_clique_qubo_matrix_ibmqx, optimizer, a)
-
              result2 = []
              for i in result_out:
                   if i == 0:
                         result2.append(1)
                   if i == 1:
                         result2.append(0)
-
              ref = subg_is_clique(result2, G)
              if ref == True:
                   ising2.append(result2.count(1))
              else:
                   ising2.append(0)
-
              print(x, ising2)
-
 print(combined_test())
 
