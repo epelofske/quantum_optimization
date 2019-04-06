20a4fdf52601f80d7bbfed3abd771db646b90417
Elijah Pelofske <45249511+epelofske@users.noreply.github.com>
2019-04-06 02:06:43 +0000

    Update max_cut_ibmqx_average_accuracy.py

--- qaoa_average/max_cut_ibmqx_average_accuracy.py
+++ qaoa_average/max_cut_ibmqx_average_accuracy.py
@@ -1,7 +1,11 @@
-from utils.graphs import *
-from utils.ibmqx_ising_qubo_qaoa import *
-from utils.qubo_ising_generators import *
-from utils.classical_solvers import *
+import os
+import sys
+d = os.path.dirname(os.getcwd())+'/utils'
+sys.path.append(d)
+from graphs import *
+from ibmqx_ising_qubo_qaoa import *
+from qubo_ising_generators import *
+from classical_solvers import *
 import networkx as nx
 import numpy as np
 import time
