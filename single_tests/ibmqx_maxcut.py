70dd906d1d577598a5050678371ac48a04d2a8cd
Elijah Pelofske <45249511+epelofske@users.noreply.github.com>
2019-04-06 02:20:19 +0000

    Update ibmqx_maxcut.py

--- single_tests/ibmqx_maxcut.py
+++ single_tests/ibmqx_maxcut.py
@@ -1,33 +1,28 @@
+import os
+import sys
+d = os.path.dirname(os.getcwd())+'/utils'
+sys.path.append(d)
 from graphs import *
-from quil2qasm import *
 from ibmqx_ising_qubo_qaoa import *
-from rigetti_ising_qubo_qaoa import *
 from qubo_ising_generators import *
-from pyquil.api import get_qc
+from classical_solvers import *
 import networkx as nx
 import numpy as np
-from dwave_qbsolv import QBSolv
-import dimod
-#from dwave.system.samplers import DWaveSampler
+import time
 from qiskit import *
 from qiskit_aqua.components.optimizers import *
 from qiskit_aqua import QuantumInstance
-from classical_solvers import *
 import warnings
-from graphs import *
 warnings.filterwarnings("ignore")
 
 def combined_test():
         G = nx.gnp_random_graph(8, 0.5, 101)
-#        G = melbourne()
         ising = []
         ising2 = []
         x = []
         print('ibmqx, max cut, slsqp, qasm simulator, no noise, gnp')
-        #print(maximum_clique(G))
         for a in range(1, 50):
              x.append(a)
-#             optimizer = POWELL()
              optimizer = SLSQP()
              result_out = solve_ibmqx_ising_qubo(G, max_cut_qubo_matrix_ibmqx, optimizer, a)
 
