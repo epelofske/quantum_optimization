c9237e58e57bf1393ee382f9689ebe43bb3e57c4
Elijah Pelofske <45249511+epelofske@users.noreply.github.com>
2019-04-06 02:21:20 +0000

    Update ibmqx_mc.py

--- single_tests/ibmqx_mc.py
+++ single_tests/ibmqx_mc.py
@@ -1,25 +1,24 @@
+import os
+import sys
+d = os.path.dirname(os.getcwd())+'/utils'
+sys.path.append(d)
 from graphs import *
-from quil2qasm import *
+from qiskit_aqua.components.optimizers import *
+from qiskit_aqua import QuantumInstance
 from ibmqx_ising_qubo_qaoa import *
-from rigetti_ising_qubo_qaoa import *
 from qubo_ising_generators import *
-from pyquil.api import get_qc
+from classical_solvers import *
 import networkx as nx
 import numpy as np
-from dwave_qbsolv import QBSolv
-import dimod
-#from dwave.system.samplers import DWaveSampler
+import time
 from qiskit import *
 from qiskit_aqua.components.optimizers import *
 from qiskit_aqua import QuantumInstance
-from classical_solvers import *
 import warnings
-from graphs import *
 warnings.filterwarnings("ignore")
 
 def combined_test():
         G = nx.gnp_random_graph(8, 0.5, 101)
-#        G = nx.algorithms.operators.unary.complement(melbourne())
         ising = []
         ising2 = []
         x = []
@@ -27,17 +26,14 @@ def combined_test():
         print(maximum_clique(G))
         for a in range(1, 50):
              x.append(a)
-#             optimizer = POWELL()
              optimizer = SLSQP()
              result_out = solve_ibmqx_ising_qubo(G, max_clique_qubo_matrix_ibmqx, optimizer, a)
-
              result2 = []
              for i in result_out:
                   if i == 0:
                         result2.append(1)
                   if i == 1:
                         result2.append(0)
-
              ref = subg_is_clique(result2, G)
              if ref == True:
                   ising2.append(result2.count(1))
